362abdaa2e084ff444e26089ad68562608d9fd17
tau <tau@megali.co.uk>
2018-11-02 12:31:47 -0300

    card token arg exposed
    Signed-off-by: tau <tau@megali.co.uk>

--- cieloApi3/creditCard.py
+++ cieloApi3/creditCard.py
@@ -3,7 +3,7 @@ from .objectJSON import ObjectJSON
 
 class CreditCard(ObjectJSON):
 
-    def __init__(self, customer_name, card_number, holder, expiration_date, security_code, brand, save_card=False):
+    def __init__(self, customer_name, card_number, holder, expiration_date, security_code, brand, card_token=None, save_card=False):
 
         self.card_number = card_number
         self.holder = holder
@@ -11,7 +11,7 @@ class CreditCard(ObjectJSON):
         self.security_code = security_code
         self.save_card = save_card
         self.brand = brand
-        self.card_token = None
+        self.card_token = card_token
         self.customer_name = customer_name
 
 
